b2adeb98655647d36d61302e2c046c71fa776296
CryptoManiac <balthazar@yandex.ru>
2019-03-02 06:06:02 +0300

    Simplify kiss99_rand_buf function

--- Optimized_Implementation/primitives/kiss99.c
+++ Optimized_Implementation/primitives/kiss99.c
@@ -30,45 +30,28 @@ void kiss99_rand_buf (kiss99_ctx *ctx, uint8_t *buf, size_t buflen) {
     } item;
 
     item *items = (item *)buf;
-    item tmp;
 
     for (size_t i = 0; i < d; ++i) {
-        uint32_t znew = 36969 * (ctx->z & 0xFFFF) + (ctx->z >> 16);
-        uint32_t wnew = 18000 * (ctx->w & 0xFFFF) + (ctx->w >> 16);
-        uint32_t mwc = (znew << 16) + wnew;
-        uint32_t shr3 = ctx->jsr ^ (ctx->jsr << 17);
-        shr3 ^= shr3 >> 13;
-        shr3 ^= shr3 << 5;
-        uint32_t cong = 69069 * ctx->jcong + 1234567;
-
-        ctx->z = znew;
-        ctx->w = wnew;
-        ctx->jsr = shr3;
-        ctx->jcong = cong;
-
-        tmp.x = (mwc ^ cong) + shr3;
-        items[i] = tmp;
+        items[i].x = kiss99_rand(ctx);
     }
 
-    for (size_t i = 0; i < m; ++i) {
-      items[d].chX[i] = tmp.chX[i];
+    if (m) {
+        item tmp;
+        tmp.x = kiss99_rand(ctx);
+        for (size_t i = 0; i < m; ++i) {
+            items[d].chX[i] = tmp.chX[i];
+        }
     }
 }
 
 uint32_t kiss99_rand (kiss99_ctx *ctx) {
-    uint32_t znew;
-    uint32_t wnew;
-    uint32_t mwc;
-    uint32_t shr3;
-    uint32_t cong;
-
-    znew = 36969 * (ctx->z & 0xFFFF) + (ctx->z >> 16);
-    wnew = 18000 * (ctx->w & 0xFFFF) + (ctx->w >> 16);
-    mwc = (znew << 16) + wnew;
-    shr3 = ctx->jsr ^ (ctx->jsr << 17);
+    uint32_t znew = 36969 * (ctx->z & 0xFFFF) + (ctx->z >> 16);
+    uint32_t wnew = 18000 * (ctx->w & 0xFFFF) + (ctx->w >> 16);
+    uint32_t mwc = (znew << 16) + wnew;
+    uint32_t shr3 = ctx->jsr ^ (ctx->jsr << 17);
     shr3 ^= shr3 >> 13;
     shr3 ^= shr3 << 5;
-    cong = 69069 * ctx->jcong + 1234567;
+    uint32_t cong = 69069 * ctx->jcong + 1234567;
 
     ctx->z = znew;
     ctx->w = wnew;
