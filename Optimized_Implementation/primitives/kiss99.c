4cc754512b78bf4ab622dbc41be59b30bf31c92e
CryptoManiac <balthazar@yandex.ru>
2019-03-02 03:54:16 +0300

    Use kiss99 to generate padding

--- Optimized_Implementation/primitives/kiss99.c
+++ Optimized_Implementation/primitives/kiss99.c
@@ -1,7 +1,7 @@
 #include "kiss99.h"
 
-void kiss99_srand (kiss99_ctx *ctx, const uint8_t *seed, int seedlen) {
-    int i;
+void kiss99_srand (kiss99_ctx *ctx, const uint8_t *seed, size_t seedlen) {
+    size_t i;
     ctx->z = 362436069;
     ctx->w = 521288629;
     ctx->jsr = 123456789;
@@ -18,6 +18,41 @@ void kiss99_srand (kiss99_ctx *ctx, const uint8_t *seed, int seedlen) {
     if (i - 3 < seedlen) ctx->z ^= seed[i - 3];
     if (i - 2 < seedlen) ctx->w ^= seed[i - 2];
     if (i - 1 < seedlen) ctx->jsr ^= seed[i - 1];
+}
+
+void kiss99_rand_buf (kiss99_ctx *ctx, uint8_t *buf, size_t buflen) {
+    size_t d = buflen / 4;
+    size_t m = buflen % 4;
+
+    typedef union {
+        uint8_t chX[4];
+        uint32_t x;
+    } item;
+
+    item *items = (item *)buf;
+    item tmp;
+
+    for (size_t i = 0; i < d; ++i) {
+        uint32_t znew = 36969 * (ctx->z & 0xFFFF) + (ctx->z >> 16);
+        uint32_t wnew = 18000 * (ctx->w & 0xFFFF) + (ctx->w >> 16);
+        uint32_t mwc = (znew << 16) + wnew;
+        uint32_t shr3 = ctx->jsr ^ (ctx->jsr << 17);
+        shr3 ^= shr3 >> 13;
+        shr3 ^= shr3 << 5;
+        uint32_t cong = 69069 * ctx->jcong + 1234567;
+
+        ctx->z = znew;
+        ctx->w = wnew;
+        ctx->jsr = shr3;
+        ctx->jcong = cong;
+
+        tmp.x = (mwc ^ cong) + shr3;
+        items[i] = tmp;
+    }
+
+    for (size_t i = 0; i < m; ++i) {
+      items[d].chX[i] = tmp.chX[i];
+    }
 }
 
 uint32_t kiss99_rand (kiss99_ctx *ctx) {
--- Optimized_Implementation/primitives/kiss99.h
+++ Optimized_Implementation/primitives/kiss99.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
 
 typedef struct kiss99_ctx kiss99_ctx;
 
@@ -11,5 +12,6 @@ struct kiss99_ctx {
     uint32_t jcong;
 };
 
-void kiss99_srand (kiss99_ctx *ctx, const uint8_t *seed, int seedlen);
-uint32_t kiss99_rand (kiss99_ctx *tcx);
+void kiss99_srand (kiss99_ctx *ctx, const uint8_t *seed, size_t seedlen);
+void kiss99_rand_buf (kiss99_ctx *ctx, uint8_t *buf, size_t buflen);
+uint32_t kiss99_rand (kiss99_ctx *ctx);
